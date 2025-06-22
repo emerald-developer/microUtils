110fcd128cc2e21b9751ed29a272c6c8031f62b7
emerlad.developer <emerlad.developer@gmail.com>
2025-06-22 06:52:50 +0000

    First app

---
+++ src/routes/apps/[app]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { page } from "$app/state";
+    let app = page.params.app;
+    import Bmi from "$lib/components/BMI.svelte";
+</script>
+{#if app == "bmi"}
+    <Bmi/>
+{/if}
