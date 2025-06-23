c42c05f3e80ea3e1dde59b588c702a0d79f93944
NoightMaster <noightmaster@gmail.com>
2025-06-23 18:58:14 +0530

    BMI

--- src/lib/components/inputs.svelte
+++ src/lib/components/inputs.svelte
@@ -17,13 +17,13 @@
 <div class="rounded-xl p-10">
   <h1 class="text-3xl font-bold text-center">BMI Calculator</h1>
   <fieldset class="fieldset">
-    <p class="fieldset-legend">Enter your mass:</p>
-    <input type="number" class="input" placeholder="Enter your mass here" bind:value={weight} />
-    <p class="label">(in kilograms)</p>
+  <p class="fieldset-legend">Enter your mass:</p>
+  <input type="text" class="input" placeholder="Enter your mass here" />
+  <p class="label">(in kilograms)</p>
 
-    <p class="fieldset-legend">Enter your height:</p>
-    <input type="number" class="input" placeholder="Enter your height here" bind:value={height} />
-    <p class="label">(in cms)</p>
+  <p class="fieldset-legend">Enter your height:</p>
+  <input type="text" class="input" placeholder="Enter you're height here" />
+  <p class="label">(in meters)</p>
 
     <button class="btn btn-primary btn-outline" on:click={getBMI}>Calculate</button>
   </fieldset>
--- src/routes/apps/[app]/bmi.svelte
+++ src/routes/apps/[app]/bmi.svelte
@@ -1,6 +1,6 @@
 <script lang=ts>
     import Inputs from "$lib/components/inputs.svelte";
-    let mass:number=0,height:number=0,bmi=$state(-1)
+    let mass,height,bmi=$state(-1)
 </script>
 
 <div class="h-screen flex flex-col">
@@ -14,7 +14,7 @@
           </div>
           <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl motion-translate-y-in-100 motion-duration-1500 ">
             <div class="card-body">
-                <Inputs/>
+                <Inputs weight={mass} height={height}/>
             </div>
           </div>
         </div>
