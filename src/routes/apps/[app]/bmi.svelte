7540785f73771423a9d5a5941c4571cd20b0924f
emerald <emerald.developer@proton.me>
2025-06-26 19:44:50 +0530

    merge frontend

--- src/routes/apps/[app]/bmi.svelte
+++ src/routes/apps/[app]/bmi.svelte
@@ -1,19 +1,22 @@
-<script>
-  import Inputs from "$lib/components/inputs.svelte";
+<script lang=ts>
+    import Inputs from "$lib/components/inputs.svelte";
+    let mass:number=0,height:number=0,bmi=$state(-1)
 </script>
 
-<div class="hero bg-base-200 flex-grow overflow-hidden">
-  <div class="hero-content flex-col lg:flex-row-reverse">
-    <div class="text-center lg:text-left">
-      <h1 class="text-5xl font-bold">Find your BMI now!</h1>
-      <p class="py-6"></p>
-    </div>
-    <div
-      class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl motion-translate-y-in-100 motion-duration-1500"
-    >
-      <div class="card-body">
-        <Inputs />
+<div class="h-screen flex flex-col">
+    <div class="hero bg-base-200 flex-grow overflow-hidden">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <div class="text-center lg:text-left">
+            <h1 class="text-5xl font-bold">Find your BMI now!</h1>
+            <p class="py-6">
+
+            </p>
+          </div>
+          <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl motion-translate-y-in-100 motion-duration-1500 ">
+            <div class="card-body">
+                <Inputs weight={mass} height={height}/>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
 </div>
