6a6a0e1644dc9b7ee86e96710caf7f16ed17bf9a
emerald <emerald.developer@proton.me>
2025-06-26 21:16:56 +0530

    color change

--- src/routes/apps/[app]/bmi.svelte
+++ src/routes/apps/[app]/bmi.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="h-screen flex flex-col">
-    <div class="hero bg-base-200 flex-grow overflow-hidden">
+    <div class="hero bg-base-100 flex-grow overflow-hidden">
         <div class="hero-content flex-col lg:flex-row-reverse">
           <div class="text-center lg:text-left">
             <h1 class="text-5xl font-bold">Find your BMI now!</h1>
@@ -12,7 +12,7 @@
 
             </p>
           </div>
-          <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl motion-translate-y-in-100 motion-duration-1500 ">
+          <div class="card bg-base-300 w-full max-w-sm shrink-0 shadow-2xl motion-translate-y-in-100 motion-duration-1500 ">
             <div class="card-body">
                 <Inputs/>
             </div>
