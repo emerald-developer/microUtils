4dbf5aef020c3ac5f0f9c90a5e3a7fc2359bfaf5
emerald <emerald.developer@proton.me>
2025-06-26 20:57:09 +0530

    bug fix 2

--- src/lib/components/inputs.svelte
+++ src/lib/components/inputs.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  let weight: number = 0;
-  let height: number = 0;
+  let weight: number;
+  let height: number;
   let bmi: number | null = null;
 
   function getBMI() {
@@ -20,7 +20,7 @@
     <p class="fieldset-legend">Enter your mass:</p>
     <input type="number" class="input" placeholder="Enter your mass here" bind:value={weight} />
     <p class="label">(in kilograms)</p>
-    
+
     <p class="fieldset-legend">Enter your height:</p>
     <input type="number" class="input" placeholder="Enter your height here" bind:value={height} />
     <p class="label">(in cms)</p>
