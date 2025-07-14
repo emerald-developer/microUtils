9fd2620be942e048c93e131776eb5394d581fb23
emerald <emerald.developer@proton.me>
2025-07-14 10:11:48 +0530

    stack shunting yard

--- src/lib/scripts/shuntingYardAlgorithm.ts
+++ src/lib/scripts/shuntingYardAlgorithm.ts
@@ -26,3 +26,80 @@ function tokenizer(input: string): string[] {
   return tokens;
 }
 
+class Token{
+  value: string;
+  type: 'number' | 'operator' | 'paren';
+  precedence?: number;
+  associativity?: 'left' | 'right';
+
+  constructor(value: string) {
+    this.value = value;
+    if (/\d/.test(value)) {
+      this.type = 'number';
+    } else if (value === '(' || value === ')') {
+      this.type = 'paren';
+    } else {
+      this.type = 'operator';
+      this.precedence = this.getPrecedence();
+      this.associativity = this.getAssociativity();
+    }
+  }
+
+  private getPrecedence(): number {
+    switch (this.value) {
+      case '+':
+      case '-':
+        return 2;
+      case '*':
+      case '/':
+        return 3;
+      case '^':
+        return 4;
+      default:
+        return 0;
+    }
+  }
+
+  private getAssociativity(): 'left' | 'right' {
+    switch (this.value) {
+      case '^':
+        return 'right';
+      default:
+        return 'left';
+    }
+  }
+
+}
+
+class Stack<T> {
+  private elements: T[] = [];
+
+  push(element: T): void {
+    this.elements.push(element);
+  }
+
+  pop(): T | undefined {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.elements.pop();
+  }
+
+  peek(): T | undefined {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.elements[this.elements.length - 1];
+  }
+
+  isEmpty(): boolean {
+    return this.elements.length === 0;
+  }
+
+  size(): number {
+    return this.elements.length;
+  }
+
+
+}
+
